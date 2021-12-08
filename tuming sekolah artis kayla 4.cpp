81067944c80ebcf8a13424b42388d18db5587cd9
Siska Hermayanti <92504989+SiskaHermayanti@users.noreply.github.com>
2021-12-08 15:50:05 +0700

    Add files via upload

---
+++ tuming sekolah artis kayla 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int t,b,u;
+	
+	cin >>t>>b>>u;
+	
+	if(t>170 && b>55 && u>=15){
+		cout << "Lolos"<<endl;
+	}else{
+		cout << "Tidak lolos"<<endl;
+	}
+	return 0;
+}
